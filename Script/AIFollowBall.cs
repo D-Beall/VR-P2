31b3d61f7d339a1c204e1e6fb7d0a4b58c3ab97f
D-Beall <31446142+D-Beall@users.noreply.github.com>
2018-03-20 15:54:03 -0500

    Files for ball follow and patrol.
    This project can use a default enemy, but I was able to add it to Ethan. I changed the AIController script with a navmesh.calculate path which determined if the ball was in the navmesh/side of the ai controller. Here is a link to the navmesh.calculate documentation.
    
    https://docs.unity3d.com/530/Documentation/ScriptReference/NavMesh.CalculatePath.html

---
+++ Script/AIFollowBall.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+    [RequireComponent(typeof(ThirdPersonCharacter))]
+    public class AICharacterControl : MonoBehaviour
+    {
+        public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+        public Transform target;                                    // target to aim for
+        private NavMeshPath path;
+
+        private void Start()
+        {
+            // get the components on the object we need ( should not be null due to require component so no need to check )
+            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+            agent.updateRotation = false;
+            agent.updatePosition = true;
+
+            path = new NavMeshPath();
+
+        }
+
+
+        private void Update()
+        {
+            if (NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path))
+                agent.SetDestination(target.position);
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+                character.Move(agent.desiredVelocity, false, false);
+            else
+                character.Move(Vector3.zero, false, false);
+        }
+
+
+        public void SetTarget(Transform target)
+        {
+            this.target = target;
+        }
+
+
+    }
+}
